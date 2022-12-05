e953ff981b80a277d7363b7f23dc2287136048f2
ashabhure <113898516+ashabhure@users.noreply.github.com>
2022-12-05 17:06:27 +0530

    task1
    Task1 is completed

--- MyProject1/src/gitDemo/DemoProgram.java
+++ MyProject1/src/gitDemo/DemoProgram.java
@@ -4,6 +4,9 @@ public class DemoProgram {
 	public static void main(String args[])
 	{
 		System.out.println("Completed demo program");
+		System.out.println("Completed demo program1");
+
+		
 	}
 
 }
---
+++ MyProject1/src/gitDemo/Task.java
@@ -0,0 +1,10 @@
+package gitDemo;
+
+public class Task {
+
+	public static void main(String[] args) {
+		System.out.println("task is completed");
+
+	}
+
+}
